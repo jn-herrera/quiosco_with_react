c3e7bebea1e3e54ab97b460739bbb312461d4999
Juan Herrera <juanalbertoherrera99@gmail.com>
2024-08-13 02:51:02 +0200

    comit

---
+++ react-quiosco/src/layouts/AuthLayout.jsx
@@ -0,0 +1,8 @@
+
+export default function AuthLayout() {
+  return (
+    <div>
+      <p>asd2</p>
+    </div>
+  )
+}
---
+++ react-quiosco/src/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, createHashRouter } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
+import Layout from './layouts/Layout'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />
+    },
+    {
+        path: '/auth',
+        element: <AuthLayout />
+    }
+])
+
+export default router
