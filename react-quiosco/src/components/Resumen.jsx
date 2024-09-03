bb82664babd521d917e4cce527c6f82d5bd25c79
Juan Herrera <juanalbertoherrera99@gmail.com>
2024-09-03 02:59:40 +0200

    commited
    finished_react_

--- react-quiosco/src/components/Resumen.jsx
+++ react-quiosco/src/components/Resumen.jsx
@@ -1,7 +1,55 @@
+
+import { formatearDinero } from '../helpers';
+import useQuiosco from '../hooks/useQuiosco'
+import ResumenProducto from './ResumenProducto';
+
 export default function Resumen() {
+
+  const { pedido, total } = useQuiosco();
+  const comprobarPedido = () => pedido.length === 0;
+
+ 
+
   return (
-    <div className="w-72">
-      Resumen
-    </div>
+
+    <aside className="w-72 h-scren overflow-y-scroll p-5">
+      <h1 className="text-4xl font-black">Mi pedido</h1>
+
+      <p className="text-lg my-5">Aqui podrás ver el resumen y totales de tu pedido</p>
+
+      <div className="py-10">
+
+        {pedido.length === 0 ? (
+
+          <p className='text-center text-2xl'>No hay elementos en el carrito</p>
+        ) : (
+
+          pedido.map(producto => (
+            <ResumenProducto 
+            key={producto.id}
+            producto = {producto} />
+
+          ))
+
+        )}
+
+      </div>
+
+      <p className='text-xl mt-10'>Total: {''} {formatearDinero(total)}</p>
+
+      <form action="" className='w-full'>
+        <div className='mt-5'>
+
+          <input type="submit" className={`${comprobarPedido() ? 
+          'bg-indigo-100' : 
+          'bg-indigo-600 hover:bg-indigo-800'} 
+          px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer`}
+          value="Confirmar pedido" disabled={comprobarPedido()}/>
+
+        </div>
+
+      </form>
+
+    </aside>
   )
 }
